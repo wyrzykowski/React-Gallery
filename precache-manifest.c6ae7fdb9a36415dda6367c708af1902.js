5d5502117bb24d9444f088247bea51b423f11747
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-05-11 13:53:07 +0200

    Updates

---
+++ precache-manifest.c6ae7fdb9a36415dda6367c708af1902.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0e6af45724b3f759917",
+    "url": "/React-Gallery/static/js/runtime~main.44864369.js"
+  },
+  {
+    "revision": "d535563f29e79b578f12",
+    "url": "/React-Gallery/static/js/main.070cc13a.chunk.js"
+  },
+  {
+    "revision": "50dee82c6326a07ebc7c",
+    "url": "/React-Gallery/static/js/2.5a58932a.chunk.js"
+  },
+  {
+    "revision": "d535563f29e79b578f12",
+    "url": "/React-Gallery/static/css/main.6d402c6e.chunk.css"
+  },
+  {
+    "revision": "50dee82c6326a07ebc7c",
+    "url": "/React-Gallery/static/css/2.063157fb.chunk.css"
+  },
+  {
+    "revision": "1510a6d2ab3a5e5380d6d91eeea1156c",
+    "url": "/React-Gallery/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wyrzykowski/React-Gallery.git/precache-manifest.9654d645c7a8d6f12c48ef629bb320ae.js"
+  "/React-Gallery/precache-manifest.c6ae7fdb9a36415dda6367c708af1902.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/wyrzykowski/React-Gallery.git/index.html", {
+workbox.routing.registerNavigationRoute("/React-Gallery/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
